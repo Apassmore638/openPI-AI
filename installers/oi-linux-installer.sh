9257dbfa3b73b908d942b821bafbc072deb31ebf
APassmore638 <abe638@gmail.com>
2024-06-30 11:38:10 -0400

    updated installer and removed unnecesary files

--- installers/oi-linux-installer.sh
+++ installers/oi-linux-installer.sh
@@ -27,11 +27,20 @@ $pyenv_root init
 $pyenv_root install $python_version --skip-existing
 $pyenv_root shell $python_version
 
+# Install necessary Python packages
 $pyenv_root exec pip install open-interpreter --break-system-packages
+$pyenv_root exec pip install tkinter pillow pyttsx3 speechrecognition pyautogui keyboard
+
 # Unset the Python version
 $pyenv_root shell --unset
+
+# Copy the updated testchat.py and image_interpreter.py to the installation directory
+install_dir="$HOME/open-interpreter"
+mkdir -p $install_dir
+cp testchat.py $install_dir
+cp image_interpreter.py $install_dir
 
 echo ""
 echo "Open Interpreter has been installed. Run the following command to use it: "
 echo ""
-echo "interpreter"
+echo "cd $install_dir && $pyenv_root shell $python_version && python testchat.py"
